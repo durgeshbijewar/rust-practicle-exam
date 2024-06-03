9522ef858aa68c55115dbb051c1ff91da59a9bf7
durgeshbijewar <137867028+durgeshbijewar@users.noreply.github.com>
2024-06-03 12:27:59 +0530

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    //variable an data type
+
+    let name = "Durgesh babulal bijewar";
+    let age: u32 = 29;
+    let is_new: bool = true;
+    let marks = [89 , 67 ,87 ,69 ,90];
+
+
+    println!("Name: {}" , name);
+    println!("Age:{}" , age);
+    println!("Is new: {}" , is_new);
+    println!("Marks: {:?}" ,marks);
+
+}
